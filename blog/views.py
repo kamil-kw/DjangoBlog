c399e4d8447be165f3bf44bb0eafc474dd063c9b
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-01-19 19:26:19 +0000

    Teh post details2

--- blog/views.py
+++ blog/views.py
@@ -20,6 +20,7 @@ class PostDetail(View):
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
+
         return render(
             request,
             "post_detail.html",
