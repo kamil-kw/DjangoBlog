1c3610ec91c2ebf44dbf7899ee73dc14508f8b54
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-01-20 20:59:58 +0000

    Authorisation - part 1

--- codestar/urls.py
+++ codestar/urls.py
@@ -1,5 +1,4 @@
 """codestar URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -16,8 +15,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('blog.urls'), name='blog_urls'),
+    path("accounts/", include("allauth.urls")),
 ]
