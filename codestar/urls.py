299dd86f88ca1b1bf7b3fb25d16efd95deee4714
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-01-23 14:48:08 +0000

    likes

--- codestar/urls.py
+++ codestar/urls.py
@@ -1,3 +1,4 @@
+
 """codestar URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
